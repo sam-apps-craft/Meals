e61a2fcbd47b42a106b403109cf2bd2458963638
sam1246 <sam@apps-craft.com>
2024-07-03 16:33:37 +0100

    Context

--- store/context/favorites-context.js
+++ store/context/favorites-context.js
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 
-const FavoritesContext = createContext({
+export const FavoritesContext = createContext({
   ids: [],
   addFavorite: (id) => {},
   removeFavorite: (id = {}),
@@ -9,27 +9,26 @@ const FavoritesContext = createContext({
 function FavoritesContextProvider({ children }) {
   const [favoriteMealIds, setFavoriteMealIds] = useState([]);
 
+  function addFavorite(id) {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  }
 
-function addFavorite(id) {
-  setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
-}
+  function removeFavorite(id) {
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  }
 
-function removeFavorite(id) {
-  setFavoriteMealIds((currentFavIds) =>
-    currentFavIds.filter((mealId) => mealId !== id)
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+  return (
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
   );
-}
-
-const value = {
-  ids: favoriteMealIds,
-  addFavorite: addFavorite,
-  removeFavorite: removeFavorite,
-};
-return (
-  <FavoritesContext.Provider value={value}>
-    {children}
-  </FavoritesContext.Provider>
-);
 }
 
 export default FavoritesContextProvider;
